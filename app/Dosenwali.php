2703d9b51f40f5e2353db7f2a79037d6601ae0b9
R <imandaputri@gmail.com>
2020-06-10 23:22:31 +0700

    Update

--- app/Dosenwali.php
+++ app/Dosenwali.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dosenwali extends Model
 {
+    // Mengkaitkan tabel dosenwali dengan tabel mahasiswa
     public function mahasiswa(){
         return $this->hasMany('App\Mahasiswa');
     }
--- app/Mahasiswa.php
+++ app/Mahasiswa.php
@@ -9,6 +9,7 @@ class Mahasiswa extends Model
 {
     protected $fillable = ['nama', 'jenis_kelamin', 'nim', 'dosenwali_id'];
 
+    // Mengkaitkan tabel dosenwali dengan tabel mahasiswa
     public function dosenwali(){
         return $this->belongsTo('App\DosenWali');
     }
